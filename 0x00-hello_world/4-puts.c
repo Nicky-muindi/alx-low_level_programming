ed2e457af22772261701491ca415a5bef0d82b6d
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-06-17 07:40:50 -0700

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ *File: 4-puts.c
+ *Auth: Nicholas M Mwanza <nicholasmuindi25@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  Return (0);
+}
