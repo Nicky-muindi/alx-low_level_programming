2477eaf4e39f1a0c50105b701d6ddd4eabb6d79c
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-06-17 08:05:03 -0700

    fixed error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,19 @@
 /*
  *File: 4-puts.c
- *Auth: Nicholas M Mwanza <nicholasmuindi25@gmail.com>
+ *Auth: Nicholas M Mwanza
  */
 
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ *main - Prints "Programming is like building a multilingual
+ *               puzzle, followed by a new line
  *
- * Return: Always 0.
+ *Return: Always 0.
  */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
 
-  Return (0);
+  return (0);
 }
