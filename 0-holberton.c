bab3dbe473f79f6a14f87a1c6222c24aa47d1d43
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-06-22 03:16:06 -0700

    program that prints Holberton, followed by a new line.

---
+++ 0-holberton.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Nicholas M Mwanza
+ */
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
