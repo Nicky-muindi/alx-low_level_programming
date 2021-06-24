c70932034161f335f7dcffb99db243bb3153b37f
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-06-24 04:32:55 -0700

    function that prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+/*
+ * File: 8-print_square.c
+ * Auth: Nicholas M Mwanza
+ */
+#include "holberton.h"
+
+/**
+ * print_square - Prints n squares according n number of times
+ * @size: The number of squares/number of times
+ *
+ * Return: empty
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
+}
