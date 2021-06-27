ec443339259963833b987049d6b43a663065f749
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-06-27 04:30:00 -0700

    function that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,16 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: Nicholas M Mwanza
+ */
 #include "holberton.h"
 /**
- * print_numbers - A function who print the numbers, from 0 to 9
+ * print_most_numbers - Print the numbers since 0 up to 9
  *
- * Description - Does not receive any parameters
+ * Description: Prints the numbers excluding 2 and 4
  *
- * Return: Does not return anything
+ * Return: The numbers since 0 up to 9
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 	for (i = 48; i < 58; i++)
