8cfca7b0cd020ae215f659c036df790c37f5d935
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-06-27 03:22:15 -0700

    fixed error

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,17 +3,12 @@
  * Auth: Nicholas M Mwanza
  * Programming in c 2021
  */
-#include "holberton.h"
-/**
- * print_numbers print numbers
- * _putchar print characters
- * return (numbers since 0 up to 9)
- */
 void print_numbers(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
-		_putchar(num + '0');
-	_putchar('\n');
-}
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i)
+			}
+		_putchar('\n');
+	}
