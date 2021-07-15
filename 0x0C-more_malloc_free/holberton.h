234c55199d3c6c080db2f5f66f86d5b658f2997e
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-07-15 13:48:49 -0700

    updated holberton.h file

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -5,7 +5,7 @@
  * File: holberton.h
  * Auth: Nicholas m Mwanza
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc_free directory.
+ *       used in the 0x0C-more_malloc_free directory.
  */
 
 int _putchar(char c);
@@ -14,5 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _strlen(char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 
 #endif
