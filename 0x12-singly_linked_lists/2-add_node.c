32299a07eff3cc6336e6e5e0e2bb34e819b758b5
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-07-29 05:58:49 -0700

    fixed error

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,43 +5,54 @@
  *str needs to be duplicated
  *You are allowed to use strdup
 */
-#include "lists.h"
+#include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning
- *            of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the list_t list.
+ * add_node - Adds a new node at the beginning of a list
+ * @head: The original linked list
+ * @str: The string to add to the node
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
+ * Return: The address of the new list or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	list_t *temp;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (dup == NULL)
+	if (head != NULL && str != NULL)
 	{
-		free(new);
-		return (NULL);
+		temp = malloc(sizeof(list_t));
+		if (temp == NULL)
+			return (NULL);
+
+		temp->str = strdup(str);
+		temp->len = _strlen(str);
+		temp->next = *head;
+
+		*head = temp;
+
+		return (temp);
 	}
 
-	for (len = 0; str[len];)
-		len++;
+	return (0);
+}
 
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
 
-	*head = new;
+	while (*s)
+	{
+		s++;
+		c++;
+	}
 
-	return (new);
+	return (c);
 }
