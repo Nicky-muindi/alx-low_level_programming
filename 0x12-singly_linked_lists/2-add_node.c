50566adba99d6907fc4b3fc6d497b33aa7428b29
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-07-29 05:39:22 -0700

    fixed style issue

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,10 @@
 /* File:2-add_node.c
-#Function that adds a new node at the beginning of a list_t list.
-
-Prototype: list_t *add_node(list_t **head, const char *str);
-Return: the address of the new element, or NULL if it failed
-str needs to be duplicated
-You are allowed to use strdup */
+ *Function that adds a new node at the beginning of a list_t list.
+ *Prototype: list_t *add_node(list_t **head, const char *str);
+ *Return: the address of the new element, or NULL if it failed
+ *str needs to be duplicated
+ *You are allowed to use strdup
+*/
 #include "lists.h"
 #include <string.h>
 
