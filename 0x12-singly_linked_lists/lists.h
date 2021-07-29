67962ba475db980196577d3c9846a271c77c2243
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-07-29 11:19:57 -0700

    source code

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x11-singly_linked_lists directory.
- */
-
 #include <stdlib.h>
 
 /**
