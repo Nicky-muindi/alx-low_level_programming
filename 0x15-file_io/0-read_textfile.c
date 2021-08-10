2d52710f6a6bb910968ac86b4777b09d65a6513c
Nicky-M254 <nicholasmuindi25@gmail.com>
2021-08-09 23:58:41 -0700

    file upload

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include "holberton.h"
-#include <stdlib.h>#
+#include <stdlib.h>
 /* read_textfile - Reads a text file and prints it to POSIX stdout."
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
