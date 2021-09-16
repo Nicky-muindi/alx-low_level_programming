d1f8306ef689aa16f9a9d5f511f13b20b860993f
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-09-16 08:53:16 -0700

    Counts the number of nodes in a doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -17,9 +17,9 @@ size_t print_dlistint(const dlistint_t *h)
 
   while (h)
   {
-          nodes++;
-          printf("%d\n", h->n);
-          h = h->next;
+         nodes++;
+         printf("%d\n", h->n);
+         h = h->next;
   }
 
   return (nodes);
